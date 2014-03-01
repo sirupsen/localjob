12e39958ab0a1a9e4a442e2108162f06fb39716c
Simon Eskildsen <sirup@sirupsen.com>
2014-03-01 13:17:30 -0500

    cli: fix for sysv

--- lib/localjob/cli.rb
+++ lib/localjob/cli.rb
@@ -2,43 +2,14 @@ require 'thor'
 
 class Localjob
   class CLI < Thor
-    option :queues,    aliases: ["-q"], type: :string, default: "localjob"
+    option :queue,     aliases: ["-q"], type: :string, default: "0x10CA110B"
     option :require,   aliases: ["-r"], type: :string, default: "."
     option :pid_file,  aliases: ["-p"], type: :string
     option :daemon,    aliases: ["-d"], type: :boolean
     desc "work", "Start worker to process jobs"
     def work
       load_environment options[:require]
-
-      queues = options[:queues].split(",")
       Localjob::Worker.new(queues, options.slice(:daemon, :pid_file)).work
-    end
-
-    desc "size", "Outputs the size of queues"
-    option :queues, aliases: ["-q"], type: :string, default: "localjob"
-    def size
-      queues = options[:queues].split(",")
-      queues.each do |queue|
-        puts "Size of /#{queue}: #{Localjob.new(queue).size}/#{msg_max}"
-      end
-    end
-
-    desc "destroy", "Destroys all queues passed"
-    option :queues, aliases: ["-q"], type: :string, default: "localjob"
-    def destroy
-      options[:queues].split(",").each do |queue|
-        Localjob.new(queue).destroy
-      end
-    end
-
-    desc "list", "Lists all queues"
-    def list
-      unless File.exists?("/dev/mqueue")
-        system "mkdir /dev/mqueue"
-        system "mount -t mqueue none /dev/mqueue"
-      end
-
-      system "ls -l /dev/mqueue"
     end
 
     private
